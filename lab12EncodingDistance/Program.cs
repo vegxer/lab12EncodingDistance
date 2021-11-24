09fa7a93addab89dd03609d88878560f9b124e18
vegxer <vegxer@mail.ru>
2021-11-24 16:26:54 +0300

    Добавьте файлы проекта.

---
+++ lab12EncodingDistance/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab12EncodingDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите путь к файлу: ");
+            List<string> codesDistances = ReadMatrix(Console.ReadLine());
+            Console.Write("Кодовые расстояния: ");
+            GetCodesDistances(codesDistances).ForEach(x => Console.Write(x + " "));
+            Console.ReadKey();
+        }
+
+        static List<int> GetCodesDistances(List<string> matrix)
+        {
+            List<int> codesDistances = new List<int>();
+
+            for (int i = 0; i < matrix.Count; ++i)
+            {
+                for (int j = i + 1; j < matrix.Count; ++j)
+                {
+                    int distance = 0;
+                    for (int k = 0; k < matrix[0].Length; ++k)
+                    {
+                        if (matrix[i][k] != matrix[j][k])
+                            ++distance;
+                    }
+                    codesDistances.Add(distance);
+                }
+            }
+
+            return codesDistances;
+        }
+
+        static List<string> ReadMatrix(string filePath)
+        {
+            List<string> matrix = new List<string>();
+            using (StreamReader reader = new StreamReader(@filePath))
+            {
+                while (!reader.EndOfStream)
+                    matrix.Add(reader.ReadLine());
+            }
+
+            return matrix;
+        }
+    }
+}
